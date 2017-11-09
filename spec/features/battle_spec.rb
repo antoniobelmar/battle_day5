7040529bccc50220fd246530f384852c450f16ea
antonio <antoniobelmardacosta@gmail.com>
2017-11-09 11:42:39 +0000

    Allows for attacking player 2

--- spec/features/battle_spec.rb
+++ spec/features/battle_spec.rb
@@ -8,7 +8,15 @@
 
     feature "seeing opponent's hit points" do
       scenario "visiting play page and seeing 60HP" do
-        visit("/play")
+        sign_in_and_play
         expect(page).to have_content "60HP"
+      end
+    end
+
+    feature "attacking" do
+      scenario "attacking player 2 and receive confirmation" do
+        sign_in_and_play
+        click_button "Attack P2"
+        expect(page).to have_content "Your attack was successful."
       end
     end
